13a12053e0edcc711466ece8b87cfed43476eb2f
bizzle <parkerjoe365@gmail.com>
2022-12-16 18:33:35 -0500

    added chart

--- scripts/chart2010.js
+++ scripts/chart2010.js
@@ -1,22 +1,41 @@
 const ctx = document.getElementById("myChart");
 
-new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1,
+var xmalhttp = new XMLHttpRequest();
+var url = "Code-Pudding/figures/interactive/popartists.json";
+xmalhttp.open("GET", url, true);
+xmalhttp.send();
+xmalhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var data = JSON.parse(this.responseText);
+    //console.log(data);
+    artist = data.popular_artists.map(function (elem) {
+      return elem.artist;
+    });
+    popularity = data.popular_artists.map(function (elem) {
+      return elem.popularity;
+    });
+    // console.log(popularity);
+
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: artist,
+        datasets: [
+          {
+            label: "Level of popularity",
+            data: popularity,
+            backgroundColor: "rgb(30,215,96)",
+            borderWidth: 1,
+          },
+        ],
       },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
       },
-    },
-  },
-});
+    });
+  }
+};
