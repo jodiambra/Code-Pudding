7d9c39de9ae71aa89e2637bde9d245e87399c5e8
bizzle <parkerjoe365@gmail.com>
2022-12-18 11:56:48 -0500

    added card class and rendering

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,49 +1,123 @@
 const initialCards = [
   {
     name: "2010 music",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+    image: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+    description: "i love 2010 yo!",
+    link: "http://127.0.0.1:5500/playlist.html",
   },
   {
     name: "2011 music",
-    link: "./images/2011.jpeg",
+    image: "./images/2011.jpeg",
+    description: "help!",
+    link: "http://127.0.0.1:5500/2011.html",
   },
   {
     name: "2012 music",
-    link: "./images/2012.jpeg",
+    image: "./images/2012.jpeg",
+    description: "longer longr longersdf it never ends sww!!!!!!!!!!!!!",
+    link: "http://127.0.0.1:5500/2012.html",
   },
   {
     name: "2013 music",
-    link: "./images/2013.jpeg",
+    image: "./images/2013.jpeg",
+    description: "i love 2013 yo!",
+    link: "http://127.0.0.1:5500/2013.html",
   },
   {
     name: "2014 music",
-    link: "./images/2014.jpeg",
+    image: "./images/2014.jpeg",
+    description: "lol",
+    link: "http://127.0.0.1:5500/2014.html",
   },
   {
     name: "2015 music",
-    link: "./images/2015.jpeg",
+    image: "./images/2015.jpeg",
+    description: "i love 2010 yo!",
+    link: "http://127.0.0.1:5500/2015.html",
   },
   {
     name: "2016 music",
-    link: "./images/2016.jpeg",
+    image: "./images/2016.jpeg",
+    description: "i love 2010 yo!",
+    link: "http://127.0.0.1:5500/2016.html",
   },
   {
     name: "2017 music",
-    link: "./images/2017.jpeg",
+    image: "./images/2017.jpeg",
+    description: "i love 2010 yo!",
+    link: "http://127.0.0.1:5500/2017.html",
   },
   {
     name: "2018 music",
-    link: "./images/2018.jpeg",
+    image: "./images/2018.jpeg",
+    description: "i love 2010 yo!",
+    link: "http://127.0.0.1:5500/2018.html",
   },
+
   {
     name: "2019 music",
-    link: "./images/2019.jpeg",
+    image: "./images/2019.jpeg",
+    description: "i love 2010 yo!",
+    link: "http://127.0.0.1:5500/2019.html",
   },
   {
     name: "2020 music",
-    link: "./images/2020.jpeg",
+    image: "./images/2020.jpeg",
+    description: "i love 2010 yo!",
+    link: "http://127.0.0.1:5500/2020.html",
   },
 ];
+
+/*
+                      Selectors
+############################################################
+*/
+const cardsContainer = document.querySelector(".cards");
+
+const cardSelector = "#card__template";
+
+console.log(cardsContainer);
+
+/*
+                      Card Class
+############################################################
+*/
+class Card {
+  constructor(data, selector) {
+    this.name = data.name;
+    this._image = data._image;
+    this._description = data._description;
+    this._link = data.link;
+    this._selector = selector;
+  }
+  _getTemplate() {
+    //prettier-ignore
+    return document.querySelector(this._selector).content.querySelector(".card").cloneNode(true);
+  }
+
+  getElement() {
+    this._element = this._getTemplate();
+
+    this._element.querySelector(
+      ".card__image"
+    ).style.backgroundImage = `url(${this._image})`;
+    this._element.querySelector(".card__subtitle").textContent = this._name;
+
+    return this._element;
+  }
+}
+
+initialCards.forEach(function (cardElement) {
+  const card = new Card(cardElement, cardSelector);
+  cardsContainer.prepend(card.getElement());
+});
+
+/*
+                      Card Rendering 
+############################################################
+*/
+
+/*
 
 const cardList = document.querySelector(".card__list");
 const cardTemplate =
@@ -65,3 +139,4 @@ initialCards.forEach((cardData) => {
   const card = getCards(cardData);
   cardList.append(card);
 });
+*/
